1c72cc8a8ba63146d51817af42d1e0398d67e80f
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-12-04 14:56:04 +0100

    post component is converted and tsconfig setup

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -30,22 +30,24 @@ export default function Avatar({
 		: "w-18 h-18 sm:w-16 sm:h-16 md:w-24 md:h-24";
 
 	return (
-		<div
-			className={`flex flex-col items-center ${className}`}
-			style={{ "--avatar-border": borderColor } as React.CSSProperties}
-		>
-			<img
-				src={src}
-				alt={name}
-				loading="lazy"
-				className={`rounded-full object-cover border-4 border-(--avatar-border) shadow-[4px_4px_8px_-3px_var(--orange-dark)] ${avatarSize}`}
-			/>
-			<p
-				style={{ "--avatar-border": textColor } as React.CSSProperties}
-				className="text-center font-bold mt-2 text-sm text-(--avatar-border)"
+		<>
+			<div
+				className={`flex flex-col items-center ${className}`}
+				style={{ "--avatar-border": borderColor } as React.CSSProperties}
 			>
-				{name}
-			</p>
-		</div>
+				<img
+					src={src}
+					alt={name}
+					loading="lazy"
+					className={`rounded-full object-cover border-4 border-(--avatar-border) shadow-[4px_4px_8px_-3px_var(--orange-dark)] ${avatarSize}`}
+				/>
+				<p
+					style={{ "--avatar-border": textColor } as React.CSSProperties}
+					className="text-center font-bold mt-2 text-sm text-(--avatar-border)"
+				>
+					{name}
+				</p>
+			</div>
+		</>
 	);
 }
