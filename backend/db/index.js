8e974f20c31729985b64c4c1f3d4349a81dc5ec1
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-12-05 16:42:15 +0100

    profile migrated

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,21 +1,21 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-// const pool = new Pool({
-//   connectionString: process.env.PGURI,
-// });
-
 const pool = new Pool({
+	connectionString: process.env.PGURI,
+});
+
+/* const pool = new Pool({
   host: 'localhost',
   port: 5432,
   user: 'postgres',
   password: '',
   database: 'innercircle',
   ssl: false
-})
+}) */
 
 export const query = (text, params) => {
-  return pool.query(text, params)
-}
+	return pool.query(text, params);
+};
