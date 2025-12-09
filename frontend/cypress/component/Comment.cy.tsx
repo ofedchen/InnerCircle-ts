35ad3e28b4ccb2125c4a3d09935fece0c1a00193
Olha Fedchenko <ofedchen@gmail.com>
2025-12-09 22:50:58 +0100

    create Comment component - empty first, developed with 4 tests

--- frontend/cypress/component/Comment.cy.tsx
+++ frontend/cypress/component/Comment.cy.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import Comment from '../../src/components/Comment.tsx'
+import React from "react";
+import Comment from "../../src/components/Comment.tsx";
 
-describe('Comment component', () => {
-  it('renders', () => {
+const comment = {
+  author: "Ben",
+  commentText: "The best video ever made, thank you",
+  date: new Date(Date.now()),
+};
+
+const date = comment.date.toLocaleString("fr-FR", {
+  dateStyle: "short",
+  timeStyle: "short",
+});
+
+describe("Comment component", () => {
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Comment />)
-  })
+    cy.mount(<Comment />);
+  });
 
-  it('')
-})
+  it("has an h3 element and a p element", function () {
+    cy.mount(<Comment />);
+    cy.get("[data-cy='comment-author']").should("exist");
+    cy.get("[data-cy='comment-text']").should("exist");
+  });
+
+  it("has an h4 element that contains date", function () {
+    const date = comment.date.toLocaleString("fr-FR", {
+      dateStyle: "short",
+      timeStyle: "short",
+    });
+    cy.mount(<Comment date={comment.date} />);
+    cy.get("[data-cy='comment-date']")
+      .should("exist")
+      .should("contain", "Date: ")
+      .should("contain", date);
+  });
+
+  it("renders correct comment author, text and date from props", function () {
+    cy.mount(
+      <Comment
+        date={comment.date}
+        author={comment.author}
+        commentText={comment.commentText}
+      />
+    );
+    cy.get("[data-cy='comment-author']").should("have.text", "Ben");
+    cy.get("[data-cy='comment-text']").should(
+      "contain",
+      "The best video ever made, thank you"
+    );
+    cy.get("[data-cy='comment-date']")
+      .should("exist")
+      .should("contain", "Date: ")
+      .should("contain", date);
+  });
+});
--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -1,7 +1,25 @@
-export default function Comment() {
+type CommentProps = {
+  author?: string;
+  date?: Date;
+  commentText?: string;
+};
 
-    return (
-        <>
-        </>
-    )
+export default function Comment({ date, author, commentText }: CommentProps) {
+  return (
+    <>
+      <h3 data-cy="comment-author">{author ? author : ""}</h3>
+      <p data-cy="comment-text">{commentText ? commentText : ""}</p>
+      {date && (
+        <h4 data-cy="comment-date">
+          Date:{" "}
+          <span>
+            {date?.toLocaleString("fr-FR", {
+              dateStyle: "short",
+              timeStyle: "short",
+            })}
+          </span>
+        </h4>
+      )}
+    </>
+  );
 }
