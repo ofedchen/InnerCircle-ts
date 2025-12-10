6d0b75e87c5002efe4bbbae18c5ebb5131eb244d
Olha Fedchenko <ofedchen@gmail.com>
2025-12-10 16:39:41 +0100

    improved types

--- backend/routes/comment.ts
+++ backend/routes/comment.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import * as db from "../db/index.js";
+import type { Comment } from "../../frontend/src/types.js";
 
 const router = Router();
 
@@ -17,28 +18,31 @@ router.get("/", async (req: Request, res: Response) => {
     const result = await db.query(query);
     res.send(result.rows);
   } catch (err: unknown) {
-    console.error("Error fetching all posts", err);
+    console.error("Error fetching all comments", err);
     res.status(500).json({
-      error: "Failed to fetch all posts",
+      error: "Failed to fetch all comments",
       message: (err as Error).message,
     });
   }
 });
 
-router.get("/:post_id", async (req: Request, res: Response) => {
-  try {
-    const result = await db.query(
-      "SELECT c.comment_id, c.comment_author, c.comment_text, c.comment_date, u.users_name FROM comment c JOIN users u ON c.comment_author=u.users_id WHERE post_id = $1",
-      [req.params.post_id]
-    );
-    res.send(result.rows);
-  } catch (err: unknown) {
-    console.error("Error fetching the post", err);
-    res.status(500).json({
-      error: "Failed to fetch the post",
-      message: (err as Error).message,
-    });
+router.get(
+  "/:post_id",
+  async (req: Request<{ post_id: string }>, res: Response) => {
+    try {
+      const result = await db.query(
+        "SELECT c.comment_id, c.comment_author, c.comment_text, c.comment_date, u.users_name FROM comment c JOIN users u ON c.comment_author=u.users_id WHERE post_id = $1 ORDER BY c.comment_date ASC",
+        [req.params.post_id]
+      );
+      res.send(result.rows);
+    } catch (err: unknown) {
+      console.error("Error fetching comments for post", err);
+      res.status(500).json({
+        error: "Failed to fetch comments for post",
+        message: (err as Error).message,
+      });
+    }
   }
-});
+);
 
 export default router;
