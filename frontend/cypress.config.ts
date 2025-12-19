55fcbf7797ddbee2fdd1a97bf518b02178f04406
Olha Fedchenko <ofedchen@gmail.com>
2025-12-19 14:08:45 +0100

    Code coverage badge (#18)
    * add code coverage badge to README in frontend, update gitignore to ignore .nyc_output
    
    * remove gitignored folder
    
    * update gitignore

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,23 +1,22 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-	component: {
-		devServer: {
-			framework: "react",
-			bundler: "vite",
-		},
-		specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
-	},
-	e2e: {
-		baseUrl: "http://localhost:5173",
-		defaultCommandTimeout: 10000,
-		pageLoadTimeout: 60000,
-		requestTimeout: 10000,
-		responseTimeout: 30000,
-		setupNodeEvents(on, config) {
-			require("@cypress/code-coverage/task")(on, config);
-			return config;
-		},
-	},
-	experimentalStudio: true,
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+  },
+  e2e: {
+    baseUrl: "http://localhost:5173",
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 10000,
+    responseTimeout: 30000,
+    setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
+      return config;
+    },
+  },
 });
